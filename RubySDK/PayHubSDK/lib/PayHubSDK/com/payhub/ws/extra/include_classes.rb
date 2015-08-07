0dbbe1b173b6cf88d0111f3243af94ab62e6e578
Agustin Breit <agustinbreit@gmail.com>
2015-08-07 11:22:41 -0300

    Changes on structure for gem creation

---
+++ RubySDK/PayHubSDK/lib/PayHubSDK/com/payhub/ws/extra/include_classes.rb
@@ -0,0 +1,3 @@
+Dir["../lib/PayHubSDK/com/payhub/ws/util/*.rb"].each {|file| require file }
+Dir["../lib/PayHubSDK/com/payhub/ws/model/*.rb"].each {|file| require file }
+Dir["../lib/PayHubSDK/com/payhub/ws/api/*.rb"].each {|file| require file }
